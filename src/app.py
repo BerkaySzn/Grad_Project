1ecd9314145d3f3052f26316459453f212f20748
BerkaySzn <brkysezan@gmail.com>
2025-02-27 17:14:07 +0300

    URL and Routes
    Added some request and created more pages. Worked with URL's and routes

--- src/app.py
+++ src/app.py
@@ -1,16 +1,52 @@
-# .venv\Scripts\Activate
+# GET REQ: curl http://127.0.0.1:5555/second_endpoint
+# POST REQ: Invoke-WebRequest -Uri "http://127.0.0.1:5555/second_endpoint" -Method Post
 
-from flask import Flask 
+# .venv\Scripts\Activate 
+
+#I installed chocolatey and CURL
+
+from flask import Flask, request, make_response 
  
 app = Flask(__name__)
 
 
+
 @app.route('/')
 def index():
-  return "Hello World"
+  response = make_response('This iss the main page\n  ')
+  response.status_code = 202
+  response.headers['content-type'] = 'text/plain',
+  return response 
 
 
+@app.route('/second_endpoint', methods = ['GET', 'POST'])
+def hello(): 
+  if request.method == 'GET':
+    return "You made a GET request"
+  elif request.method == 'POST':
+    return "You made a POST request"
+  else:
+    return "You hacked the system"
+
+
+
+@app.route('/greet/<name>')
+def greet(name):
+  if 'greeting' in request.args.keys() and 'name' in request.args.keys():
+    greeting = request.args.get('greetings')
+    name = request.args.get('name')
+    return f'{greeting}, {name}'
+  else:
+    return 'Some parameters are missing'
+
+@app.route('/handşe_url_params')
+def handle_params():
+  return str(request.args)
+
+
+ 
 if __name__ == '__main__':
   app.run(host = '0.0.0.0', port = 5555, debug = True)
 
 
+  
