a9a171b21a0c4ae1b517464b4aeddae5fd34585f
BerkaySzn <brkysezan@gmail.com>
2025-03-10 10:59:29 +0300

    commit

--- src/application/app.py
+++ src/application/app.py
@@ -1,30 +1,27 @@
 # GET REQ: curl http://127.0.0.1:5555/second_endpoint
 # POST REQ: Invoke-WebRequest -Uri "http://127.0.0.1:5555/second_endpoint" -Method Post
 
-# v
+#
 
-#I installed chocolatey and CURL,
 
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-
-db = SQLAlchemy()
+from routes import app_bp
 
 def create_app():
-    app = Flask(__name__, template_folder='templates')
+    app = Flask(__name__, template_folder='../templates')
 
-    
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mssql+pyodbc://@DESKTOP-OPP7BQ4\\MSSQLSERVER01/Grad_Project_DB?driver=ODBC+Driver+17+for+SQL+Server;TrustServerCertificate=yes;trusted_connection=yes'
+    app.config['DATABASE_URI'] = (
+        'DRIVER={ODBC Driver 17 for SQL Server};'
+        'SERVER=DESKTOP-OPP7BQ4\\MSSQLSERVER01;DATABASE=Grad_Project_DB;'
+        'TrustServerCertificate=yes;Trusted_Connection=yes;'
+    )
 
-
-    app.config['SQLALCHMEY_TRACK_MODIFICATIONS'] = False
-    
-    db.init_app(app)
-
-    migrate = Migrate(app, db)
+    app.register_blueprint(app_bp)
 
     return app
 
-import pyodbc
-print(pyodbc.drivers())
+app = create_app()
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
