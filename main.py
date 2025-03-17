1eb6c20519a2a2da3eb5805b1056567a963a8c72
BerkaySzn <brkysezan@gmail.com>
2025-03-17 17:08:56 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,4 +4,65 @@ app = create_app()
 
 if __name__ == '__main__':
     app.run(debug=True)
-  
+
+
+
+TL:DR - CL1 Biyolojik Bilgisayar
+
+
+
+Cortical Labs şirketi geçtiğimiz günlerde oldukça fütüristik bir gelişmeye imza attı. 
+
+
+
+2 Mart 2025'de MWC(Mobile Web Congress) 2025 etkinliğinde tanıtılan CL1 isimli bilgisayar, farklılığını silikon tabanlı yapay zeka sistemlerine alternatif olarak, canlı ve bir yaşam döngüsüne sahip insan beyin hücrelerini silikon çiplere adapte ederek ortaya koyuyor.
+
+
+
+Bu yaklaşımı BI(Biological Intelligent) olarak adlandıran Cortical Labs, AI sistemlere göre kayda değer bir enerji tasarrufu, daha az veri ile işlem yapabilme, çok daha hızlı öğrenme ve adaptasyona uygun yapısı ile öne çıkacağını iddia ediyor.
+
+
+
+İnsan kanından ya da derisinden alınan kök hücreleri beyin nöronlarına dönüştüren şirket, tasarladıkları bilgisayarın içindeki özel ayarlanmış yaşam koşulları ile bu hücreleri 6 ay boyunca hayatta tutabiliyor.
+
+
+
+2022 yılında yaptıkları çalışmalarda sisteme başarılı şekilde pong oynatabilen ekip, tasarladıkları biOS isimli işletim sistemleri ile donanımın kendisini satın almadan, bulut sistemleri WaaS(Wetware as a Service) ile nöronları programlayabileceğimizi vaat ediyor.
+
+
+
+2025'in sonlarına doğru 35.000 dolar fiyat etiketi ile satışa sunulacak dünyanın ilk biyolojik bilgisayarı, etik açıdan büyük tepkilere yol açtı. Cortical Labs'a göre farkındalık sahibi olmayan bu sistemi yaratırken ki amaçları sürekli acı çeken bir sistemden ziyade nöronların hesaplama gücünden faydalanmak.
+
+
+
+Hon Weng Chong kısa vadede tıp alanında ilaç testleri, hastalık tespitleri gibi alanlarda kullanılmasını ama uzun vadede asıl amaçlarının yeni bir hesaplama biçiminin öncüleri olmak istediklerini söyledi.
+
+
+
+Şirket herhangi bir tartışmaya ya da incelemeye tamamen açık olduklarını belirtiyor ve kurumların regülasyonlarına uyduklarını söylüyor ama kamuoyu için bu tarz bir yeniliği kabullenmek zaman alacaktır. Hesaplamanın geleceği olur mu tartışılır ama teknolojinin ilerlemesinin durdurulamayacağı gerçeğini de göz önünde bulundurursak kayda değer bir gelişme olduğu kesin.
+
+
+
+Kendi uygulamalarına entegre etmek isteyenler için Python API da mevcut: https://github.com/Cortical-Labs/cl-api-doc/
+
+
+
+I want to personally congratilade Hon Weng Chong, Brett Kagan and the Cortical Labs team for their outstanding achievement.
+
+
+
+Kaynakça: 
+
+-https://www.linkedin.com/in/hochong/
+
+-https://www.linkedin.com/company/cclabs-ai/
+
+-https://corticallabs.com/
+
+-https://turkiye.ai/dunyanin-ilk-sentetik-biyolojik-zekasi-cl1/ Yasin Demirkaya
+
+-https://x.com/IterIntellectus/status/1896951102794498284
+
+-https://www.youtube.com/watch?v=3KeC8gxopio
+
+-https://www.youtube.com/watch?v=nyQn4FNVvrQ
